1e9e5f4e409ab2b64e936709f5f224fb36c6a915
Joe Hesse-Withbroe <joseph.hesse-withbroe@colorado.edu>
2025-09-09 09:44:12 -0600

    print_fires.py updated to correctly use my_utils.get_column()

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,8 @@
+import my_utils
+
 country='United States of America'
-county_column = 1
-fires_column = 4
-file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
+country_column = 0
+fires_column = 3
+file_name = "Agrofood_co2_emission.csv"
+fires = my_utils.get_column(file_name,country_column,country,fires_column)
 print(fires)
