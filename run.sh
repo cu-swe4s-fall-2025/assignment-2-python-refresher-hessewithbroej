931b1f2678663e716a097baf44297c9afe0fcca5
Joe Hesse-Withbroe <joseph.hesse-withbroe@colorado.edu>
2025-09-17 12:02:33 -0600

    Improved my_utils with basic exception handling in the most error-prone areas. Cleaned up all files to follow pep-8 guidelines

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+#runs successfully using default values 
 python print_fires.py
+
+#errors when trying print fires column as a list of ints 
+python print_fires.py --fires_column 0
+
+#errors when trying to read a nonexistent csv fil
+python print_fires.py --file_name BadFile.csv
