c4e856e53a07d3b73e6231adc3f813982c8b96ee
Joe Hesse-Withbroe <joseph.hesse-withbroe@colorado.edu>
2025-10-07 11:30:31 -0600

    fixed pathing

--- test/test_my_utils.py
+++ test/test_my_utils.py
@@ -11,9 +11,9 @@ import my_utils
 
 class TestMyUtils(unittest.TestCase):
     def test_get_column(self):
+        print("Testing mssg")
         # test return type
         self.assertIsInstance(my_utils.get_column(
-            './Assignments/assignment-2-python-refresher-hessewithbroej/'
             'Agrofood_co2_emission.csv', 0, 'Zimbabwe', result_column=1), list)
 
     # calc_mean tests
@@ -87,3 +87,7 @@ class TestMyUtils(unittest.TestCase):
                 self.assertTrue(my_utils.calc_stdev(arr) == 0)
             else:
                 self.assertTrue(my_utils.calc_stdev(arr) > 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
